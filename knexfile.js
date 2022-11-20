e918887ce2af1c590da2421c5627eb4091f77009
AstroAntics <vucinicm@asbindia.org>
2022-11-20 14:06:44 +0200

    Remove knexfile from gitignore, clean up user & post listing

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+// Update with your config settings.
+// Moved to non Gitignore because we could host this on another system
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'notreddit',
+      user:     'postgres',
+      password: 'root'
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'notreddit',
+      user:     'postgres',
+      password: 'root'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'notreddit',
+      user:     'postgres',
+      password: 'root'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
